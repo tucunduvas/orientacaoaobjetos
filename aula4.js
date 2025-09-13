c7139f4dc18d55d608aa1ce10cf3e84d2add3057
GABRIELA SANTOS TUCUNDUVA <gabriela.tucunduva_e@C111089.pdc-academico.ifms.tl>
2025-09-12 22:47:58 -0400

    exercicios

--- aula4.js
+++ aula4.js
@@ -2,7 +2,8 @@
 // nao é uma cópia é o endereço em que está na memória
 // tipo primtivo copia do valor, instancia é referencia de memoria
 class Pessoa{
-    nome;
+    // #nome; para ser privado
+    nome; 
     dataNasc;
     cpf;
 
@@ -40,3 +41,42 @@ x = 10;
 pessoa2 = pessoa1;
 
 pessoa2.apresentar();
+
+let pessoa4 = new Pessoa;
+let pessoa5 = new Pessoa;
+pessoa4.nome = 'Emilia';
+pessoa5.nome = 'Lulu';
+pessoa4.dataNasc = '23/07/1999';
+pessoa4.cpf = '09812367600';
+pessoa5.dataNasc = '24/08/1973';
+pessoa5.cpf = '08065412388';
+
+let carro = {pessoa1, pessoa2, pessoa3, pessoa4, pessoa5};
+console.log(carro);
+
+// class Onibus{
+//     modelo;
+//     cor;
+
+//     entrar_passageiros(){
+//         let 
+//     }
+// }
+
+
+
+
+// pessoa2 = pessoa1;
+// pessoa1.nome = 'Caio';
+// console.log('Pessoa 2 = pessoa 1');
+// pessoa2.apresentar();
+// assim aparece caio na pessoa 2
+
+
+// pessoa1.nome = 'Ana';
+// pessoa1.apresentar();
+// pessoa2.nome = pessoa1.nome;
+// pessoa1.nome = 'Caio';
+// console.log('Pessoa 2 = pessoa 1');
+// pessoa2.apresentar();
+// assim fica ana 
